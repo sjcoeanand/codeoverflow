8b9a479a9653a451e534dc2626a593d44ea7fe1b
A Shendage <shendageanand7@gmail.com>
2019-04-12 01:18:10 +0530

    sequilize update

--- server/src/bin/www.js
+++ server/src/bin/www.js
@@ -1,5 +1,5 @@
 import app from '../server-app';
-app.set('port', process.env.PORT || 2083)
+app.set('port', process.env.PORT || 5001)
 
 let server = app.listen(app.get('port'), async function () {
   console.log('Express server listening on port ' + server.address().port)
